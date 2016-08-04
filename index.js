a12ab443ca15773daa7827f8f64a645a8f64d86e
Tsi Lung <tsi.lung@gmail.com>
2016-08-04 21:01:19 +0000

    Done.

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var katzDeliLine = []
 
 function takeANumber(array, str) {
   array.push(str);
-  return `Welcome, ${str}.  You are number ${array.length} in line.`;
+  return `Welcome, ${str}. You are number ${array.length} in line.`;
 }
 
 function currentLine(array) {
   if (array.length === 0) {
-    return "The line is currently empty"
+    return "The line is currently empty."
   } else {
       var result = []
       for (var i = 0; i < array.length; i++) {
-        result[i] = (array.indexof(i)+1) + ". " + array[i] + " ";
+        result[i] = (array.indexOf(array[i])+1) + ". " + array[i];
       }
-      return `The line is currently: ${result.join()}`;
+      return `The line is currently: ${result.join('')}.`;
   }
 }
 
