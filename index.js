4ed85827645a8a97063632d13f1c49c0334637d2
Komediruzec  Dalziher <komediruzecki.2015@gmail.com>
2016-08-10 14:26:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+function takeANumber(curLine, newGuest){
+    var temp = curLine.lenght;
+    return `You are ${temp + 1} in line. `;
+}
--- test/index-test.js
+++
@@ -1,4 +0,0 @@
-function takeANumber(curLine, newGuest){
-    var temp = curLine.lenght;
-    return `You are ${temp + 1} in line. `;
-}
