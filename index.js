54e87b36759f1c46ea48d0824cb980945d519b5c
Komediruzec  Dalziher <komediruzecki.2015@gmail.com>
2016-08-10 15:18:20 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function takeANumber(curLine, newGuest){
 }
 
 function nowServing(curLine){
+
   var str = "There is nobody waiting to be served!"
   if(curLine.length <= 0){
     return str
   }
+  var strNext = curLine[0]
+  curLine.shift()
+  return `Currently serving ${strNext}`
 
 }
