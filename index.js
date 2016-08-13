a11b362408654dce871cb7c3ec093330094d841b
Karina Bartz <ksbartz@gmail.com>
2016-08-13 15:00:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeli = [];
+
+function takeANumber (katzDeliLine, name){
+  for (var i = 0, l = katzDeliLine.length; i < l; i++) {
+    return katzDeliLine[i+1]
+};
+
+function nowServing (katzDeliLine[i]){
+  i === 0 ? katzDeliLine.push(`There is nobody waiting to be served.`) : katzDeliLine.shift()
+};
+
+function currentLine (name, katzDeliLine){
+  i === 0 ? katzDeliLine.push(`There is nobody waiting to be served.`) : katzDeliLine.shift()
+}
