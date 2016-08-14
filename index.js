66afd0f93ce5d545eaa082b622ca61a9b627bba3
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-14 10:54:36 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function takeANumber(katzDeliLine, newCustomer){
   katzDeliLine.push(newCustomer);
   return `Welcome, ${newCustomer}. You are number ${katzDeliLine.indexOf(newCustomer) + 1} in line.`;
 };
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.count < 1){
+      return "There is nobody waiting to be served!";
+  }
+  else{
+    var name = katzDeliLine.shift();
+    return `Currently serving ${name}.`;
+  }
+
+
+};
