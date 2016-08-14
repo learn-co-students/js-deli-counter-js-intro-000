f5656410cb8620f23cc01f40a6ef69b36e1615cf
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-14 11:31:36 +0000

    Done.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(katzDeliLine){
   else{
     var count = katzDeliLine.length;
     for (var i=0; i<count; i++){
-      string.push(`${i+1}. ${katzDeliLine[i]} `);
+      string.push(`${i+1}. ${katzDeliLine[i]}`);
     }
     return `The line is currently: ${string}`;
   }
