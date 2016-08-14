91e9c9d94b5781eb63a6de1ff20406a975877d39
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-14 10:31:35 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, newCustomer){
   katzDeliLine.push(newCustomer);
-  return katzDeliLine.indexOf(newCustomer) + 1;
-
+  return `Welcome, ${newCustomer}. You are number ${katzDeliLine.indexOf(newCustomer) + 1} in line.`;
 };
