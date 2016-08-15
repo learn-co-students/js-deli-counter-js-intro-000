b34ca02e88d86cb1fc39df83979fcdbe7e49f50c
David Morse <webdevdjm@gmail.com>
2016-08-15 12:21:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
-  var numPeopleInLine = katzDeliLine.length;
-  return numPeopleInLine;
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
 }
