5f3277d56ba113d4df0a3f127eef91537c534f2e
David Morse <webdevdjm@gmail.com>
2016-08-15 12:27:04 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
 }
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length == 0){
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return katzDeliLine[0];
+  }
+}
