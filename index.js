5fd649258541bbd7a6ae6cf7e17949999f5f185b
Joanna Huang <joannathhuang@gmail.com>
2016-08-25 15:50:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeANumber(katzDeliLine, name){
+    katzDeliLine.push(name)
+    return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+  }
+
+function nowServing(deliLine){
+    return (deliLine)
+    deliLine.shift();
+    
+  }
