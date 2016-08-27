b55b90ed23066fc1995bfeda2db00b2e0d0c5849
Cindy Gao <cindygao@hotmail.com>
2016-08-27 02:03:32 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name){
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(){
+function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   }
