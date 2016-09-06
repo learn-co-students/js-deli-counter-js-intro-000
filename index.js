9acf5e7daedd129fc793fd5e7b891888abc6dad0
David Ganthier <david.ganthier@gmail.com>
2016-09-06 18:11:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 var katzDeli = [];
 
 /* deli takeANumber adds a person to the line:
-ReferenceError: takeANumber is not defined */
-
-
+ReferenceError: takeANumber is not defined -->*/
 function takeANumber(katzDeliLine, personName) {
    katzDeliLine.push(personName);
-return `'Welcome, ${personName}. You are number ${katzDeliLine.length} in line.'`;
+return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`;
+}
+
+/* deli nowServing returns the line is empty when no on is on line:
+ReferenceError: nowServing is not defined -->*/
+
+/* deli nowServing returns an announcement about the person it is serving, and
+shifts the line: ReferenceError: nowServing is not defined  -->*/
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+
+  else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+}
+
+
+/*<-- There is an error in index-test.js. Look under 'describe('nowServing', ()'
+'when no on is on line'. */
+
+/*<-- There is an error in terminal test "nowServing" as well "when no on is on
+line" */
+
+/* deli currentLine(line) returns "The line is currently empty." if no one is in
+line: ReferenceError: currentLine is not defined -->*/
+
+/* deli currentLine(line) says who is in line when there are people waiting:
+ReferenceError: currentLine is not defined -->*/
+
+function currentLine() {
+  var newCurrentLine = currentLine(katzDeliLine);
+  return `The line is currently: ${newCurrentLine}`;
+/*
+  if (newCurrentLine >= 1) {
+    return `The line is currently: ${newCurrentLine}`;
+  }
+
+  else {
+    return "The line is currently empty.";
+  }
+*/
 }
