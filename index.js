7c0ede237e34d76ee8cf71a063311739da35f4cb
Rent Sidon <rsidon@icloud.com>
2016-09-08 20:42:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var katzDeliLine = [];
+
+function takeANumber(deliLine, customer) {
+
+ deliLine.push(customer)
+  return `Welcome, ${customer}. You are number ${deliLine.length} in line.`
+};
+
+/*
+2. Build a function `nowServing`. This function should return the next person in
+ line and then remove them from the line. If there is nobody in line, it should return
+  "There is nobody waiting to be served!"
+*/
+function nowServing(deliLine) {
+if (deliLine.length === 0) {
+  return `There is nobody waiting to be served!`
+}
+else {
+  return `Currently serving ${deliLine.shift()}.`
+}
+};
+
+// currentLine()  returns `"The line is currently: 1. Ada 2. Grace"`.  If nobody in line, return `"The line is currently empty."`
+
+function currentLine(deliLine, customer) {
+  if (deliLine.length === 0) {
+   return `The line is currently empty.`
+}
+else {
+  var lineString = "The line is currently: "
+      for (var i = 1; deliLine.length >= 1; i++) {
+        lineString.push(`${i}. ${deliLine[i]}`)
+        return lineString
+    }
+  }
+}
