8d5cb32ee559fa5250bd0983c657eeb3bb7cc6cb
Fernando Gomez <fhergomez37@gmail.com>
2016-09-26 08:06:42 +0000

    add function currentLine

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ function takeANumber(deliLine, name){
 function nowServing(deliLine){
   if (deliLine.length === 0){
     return "There is nobody waiting to be served!"
-  } else {
-    return `Now serving ${deliLine[i]}`;
-    deliLine.slice(0,deliLine.length-1);
   }
+  return `Currently serving ${deliLine.shift()}.`;
+}
 
+function currentLine(deliLine){
+  if(deliLine.length === 0){
+    return "The line is currently empty.";
+  } else {
+    for (var i = 0; i > deliLine.length;i++){
+      deliLine.shift(i);
+    }
+    return `The line is currently: ${i+1}. ${deliLine[i]}`;
+  }
 }
