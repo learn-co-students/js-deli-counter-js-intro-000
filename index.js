2f79a4accc3b471ab97efda8f28720fd39672397
Fernando Gomez <fhergomez37@gmail.com>
2016-09-26 07:33:26 +0000

    add takeANumber function

---
+++ index.js
@@ -0,0 +1,8 @@
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  for(var i = 0;i > katzDeliLine.length;i++){
+    return `Welcome, ${name}. You are number ${katzDeliLine[i+1]} in line.`;
+  }
+
+}
