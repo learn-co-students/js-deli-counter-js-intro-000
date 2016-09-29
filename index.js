d7b4009ef2cf4ca2bc465c8f2e205ef6e088c179
crisam degracia <crisamdegracia@gmail.com>
2016-09-29 20:31:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 function takeANumber(katzDeliLine,katzDeli){
 
-for ( var i = katzDeli.length ;  katzDeli.length < i ; i++){
+for ( var  i = katzDeliLine.length ;  katzDeliLine.length < i ; i++){
 katzDeliLine.push(katzDeli)
 }
-return `Welcome, ${katzDeli}. You are number ${katzDeli.length} in line.`
+return `Welcome, ${katzDeli}. You are number ${i} in line.`
 }
