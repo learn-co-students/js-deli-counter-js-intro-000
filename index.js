d7173a4d4a7e195862d060a853308005830d50eb
crisam degracia <crisamdegracia@gmail.com>
2016-09-29 16:30:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function takeANumber(katzDeliLine,katzDeli){
+
+
+for ( var i = katzDeli.length ;  katzDeli.length < i ; i++){
+katzDeliLine.push(katzDeli)
+}
+return `Welcome, ${katzDeli}. You are number ${i} in line.`
+}
