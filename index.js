b6f9be07f109582e6cb6cee34616c4ab67d52ba8
L North <lnordell@gmail.com>
2016-10-02 12:16:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeliLine = [];
+
+function takeANumber (katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson);
+  var yourPosition = katzDeliLine.indexOf(newPerson)+1;
+  return `Welcome, ${newPerson}. You are number ${yourPosition} in line.`
+  //or return `Welcome, ${newPerson}. You are number ${katzDeliLine.indexOf(newPerson) + 1} in line.`;
+}
+
+function nowServing (line){
+  while (line.length > 0){
+// this also works >>    return `Currently serving ${katzDeliLine.shift()}.`;
+  var customerName = line[0];
+  line.shift();
+  return `Currently serving ${customerName}.`;
+// for some reason katzDeliLine.shift() at the end of the loop didnt' work. have to figure out why later.
+  }
+  return "There is nobody waiting to be served!"
+}
+
+function currentLine(line){
+  if (line.length > 0){
+    var myList = [];
+    for (var counter = 0; counter < line.length; counter++) {
+      myList.push (` ${counter+1}. ${line[counter]}`)
+      }
+    return `The line is currently:${myList}`
+    }else{
+    return `The line is currently empty.`
+  }
+}
