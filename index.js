ee0142b86ad60eb9c9a086c4af409c4a96b2dc6e
Swan Htet <swan.htet@yahoo.com>
2016-11-25 22:19:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeiLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+function takeANumber(line, name){
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length===0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var temp = line[0];
+    line.shift();
+    return `Currently serving ${temp}.`
+  }
+}
+
+function currentLine(line){
+  if (line.length===0){
+    return "The line is currently empty."
+  }
+ else{
+  var numberAndName = [];
+  for(var i=0;line.length>i; i++){
+    numberAndName.push(`${i+1}. ${line[i]}`);
+  }
+  var toPrint = numberAndName.join(', ');
+  return `The line is currently: ${toPrint}`
+ }
 }
