e5a2e280e24efc24bc1911066f3e47ed880dc126
Jon Roethke <jon@vidroll.com>
2016-12-18 19:58:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine == 0) {
+    return "The line is currently empty."
+  } else {
+    var line = "";
+    for (var i = 1; i < katzDeliLine.length; i++) {
+      line = line + i + ". " + katzDeliLine[(i-1)] + ", "}
+      return ("The line is currently: " + line + i + ". " + katzDeliLine[(i-1)])
+    }
+  }
