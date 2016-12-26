f4bde45790da75e98d54e5ffdc660b6c870b16c5
Gopal Masilamani <haigopal@gmail.com>
2016-12-26 02:37:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function takeANumber(line, name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line){
+  if (line.length===0)
+  {
+    return `There is nobody waiting to be served!`
+  }
+}
+
+function nowServing(line, name){
+
+  if (line.length<=0)
+  {
+    return `There is nobody waiting to be served!`
+  }
+  else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line){
+  if (line.length<=0)
+  {
+    return `The line is currently empty.`
+  }
+  else {
+    const numbersAndNames = []
+
+    for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+    return `The line is currently: ${numbersAndNames.join(', ')}`
+  }
+}
