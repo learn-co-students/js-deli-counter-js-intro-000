5551452cf772d3a00ffa3d4fc4ff110e14766845
Peter Kim <peteryunkim@gmail.com>
2017-01-11 19:51:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+var katzDeliLine = [];
+
 function takeANumber(katzDeliLine, name) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    katzDeliLine.push(`Welcome, ${name}. You are number ` + i + " in line.")
-  }
-  return katzDeliLine
+    katzDeliLine.push(name)
+    for (var i = 0; i < katzDeliLine.length; i++){
+    return `Welcome, ${name}. You are number ` + katzDeliLine.length + ` in line.`
 }
-/*
-function nowServing(katzDeliLine){
-  if (katzDeliLine.length !=== 0) {
-    katzDeliLine.shift()
-      return 'Currently serving ${[katzDeliLine[0]]}';
-  } else (katzDeliLine.length === 0) {
-    return "There is nobody left to be served!"
-  }
+}
+
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+      return "Currently serving " + katzDeliLine[0] + "."; katzDeliLine.shift()
+
   }
 }
-*/
+
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var a = []
+    for ( var i = 0; i < katzDeliLine.length; i++) {
+      a.push(katzDeliLine);
+        return `The line is currently: ` + i + 1 + `. ${katzDeliLine}`
+  }
+
+  }
+}
