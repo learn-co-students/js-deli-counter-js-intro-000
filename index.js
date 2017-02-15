ec304d673ba6e37d07102ea355da4c77ca952520
Gil Siegel <packerplayer2268@aol.com>
2017-02-15 21:15:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeli = []
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+function nowServing(katzDeliLine) {
+  var nextName = katzDeliLine.shift()
+  if(katzDeliLine.length > 0) {
+    return(`Currently serving ${nextName}.`)
+  } else {
+    return("There is nobody waiting to be served!")
+  }
+}
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return("The line is currently empty.")
+  } else {
+    var theLine = "The line is currently: "
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      theLine += `${i+1}. ${katzDeliLine[i]}`
+      if (i < katzDeliLine.length - 1) {
+        theLine += `, `
+      }
+    }
+    return theLine
+  }
+}
