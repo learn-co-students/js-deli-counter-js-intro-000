4570d032b6fde3a6f0fc04d36abc776c135f110a
joseph Harris <jay.pay92@ymail.com>
2017-03-03 04:57:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 var katzDeli = [];
 
-function takeANumber(){
-  katzDeliLine = []
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+
+for (var i = 0; i < array.length; i++) {
+  return array[i]
+}
+
+
+}
+
+function nowServing(n) {
+  const deliLine = []
+
+  for (var i = 0; i < deliLine.length; i++) {
+    return `Currently serving ${deliLine[i]}.`
+  }
+
+  return "There is nobody waiting to be served!"
+
+
+}
+
+function currentLine(){
+  const array = []
+
+
+
 }
