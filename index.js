072b298ab4d258537556ac648d18acd94b2161e3
Hou In Choi <hichoi17@outlook.com>
2017-03-27 22:14:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var katzDeli=[]
 function takeANumber(katzDeli,people){
   var temp=[]
-  if(katzDeli.length===0){
+  //if(katzDeli.length===0){
   katzDeli.push(people)
-    for(let i=0;i<katzDeli.length;i++){
+    /*for(let i=0;i<katzDeli.length;i++){
       temp.push(`Welcome, ${katzDeli[i]}. You are number ${i+1} in line.`)
-    };
+    };*/
+  /*
   }else{
     katzDeli.push(people)
         temp.push(`Welcome, ${katzDeli[katzDeli.length-1]}. You are number ${katzDeli.length} in line.`)
-  }
-  
-  return temp
-  
+  }*/
+  return `Welcome, ${katzDeli[katzDeli.length-1]}. You are number ${katzDeli.length} in line.`
+  //return temp
+
 }
 
 function nowServing(line){
@@ -27,15 +28,18 @@ function nowServing(line){
 function currentLine(line){
 
   if(line.length>0){
-    var temp='The line is currently:'
+    //var temp='The line is currently:'
+    var array=[]
     for(let i =0;i<line.length;i++){
+      /*
       if(i!=line.length-1){
         temp +=` ${i+1}. ${line[i]},`
       }else{
         temp +=` ${i+1}. ${line[i]}`
-      }
+      }*/
+      array.push(`${i+1}. ${line[i]}`)
     }
-    return temp
+    return `The line is currently: ${array.join(", ")}`
   }else{
     return `The line is currently empty.`
   }
