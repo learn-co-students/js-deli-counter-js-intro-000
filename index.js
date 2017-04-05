8c130bd90224ff15e85365898ee0ad4963f4df06
Annee Barrett <ahuang852@gmail.com>
2017-04-05 20:22:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var katzDeli = []
+var katzDeliLine = []
 
-function takeANumber(name) {
-  katzDeli.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing() {
-
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }  else {
+    return "Currently serving " +  katzDeliLine.shift() + "."
+  }
 }
 
-function currentLine() {
-
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var numbersAndNames = [];
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      numbersAndNames.push((i + 1) + ". " + katzDeliLine[i]);
+    }
+    return "The line is currently: " + numbersAndNames.join(", ")
+  }
 }
