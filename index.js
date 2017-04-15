97056ba8a2ef0f42daa102e71bc9574dc802648e
sungyun kim <kimsungyun7@hotmail.com>
2017-04-15 23:34:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var katzDeli = [];
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
-}
-
-function nowServing(name) {
-  if (name.length === 0) {
-    return "There is nobody waiting to be served!";
-  } else {
-    name.shift(name[0]);
-    return "Currently serving "+ "Steven" + ".";
-  }
-}
-
 function currentLine(line) {
-  if (line.length === 0) {
-    return "The line is currently empty.";
-  } else {
-    for(var i = 0; i < line.length; i++) {
-      line += (i + 1) + ". " + line[i] + ", "
-    }
-    line = line.slice(0, line.length-2)
-    return "The line is currently: " + line
+  if (!line.length) {
+    return "The line is currently empty."
   }
-}
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
