a602e0620c125912b4cc74ccd62a292f22d94f9d
Angela Whitehead <angelacwhitehead@gmail.com>
2017-04-19 17:04:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  for (var i = 0; i < katzDeliLine.length; i++)
+  name.push
+  return (`Welcome, ${name[i]}. You are number ${katzDeliLine + 1} in line.`)
+}
