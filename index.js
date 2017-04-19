bc8790023352b5e8ef6ea2ac1f80343f7d311c97
Matt Faircloth <faircloth.matthew@gmail.com>
2017-04-19 00:10:57 +0000

    Done.

--- index.js
+++ index.js
@@ -13,12 +13,17 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return ("The line is currently: ");
-  }  else {
+  if (!katzDeliLine.length) {
     return "The line is currently empty."
   }
-    for(let i = 0; i < katzDeliLine.length; i++) {
-      return(`${i + 1}. ${katzDeliLine[i]}`);
+
+  const nameList = []
+
+  for (let i = 0, l = katzDeliLine.length; i < l; i++) {
+    nameList.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+
+  return `The line is currently: ${nameList.join(', ')}`
 }
+
 
