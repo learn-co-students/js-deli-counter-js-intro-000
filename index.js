422cc990c66eb79993575738ed0b5eed3dfae988
Angela Whitehead <angelacwhitehead@gmail.com>
2017-04-19 18:35:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-var katzDeliLine = [];
-
 function takeANumber(katzDeliLine, name) {
-  for (var i = 0; i < katzDeliLine.length; i++)
-  name.push
-  return (`Welcome, ${name[i]}. You are number ${katzDeliLine + 1} in line.`)
+  katzDeliLine.push(name)
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing(deliLine) {
+  if (deliLine.length > 0) {
+    return (`Currently serving ${deliLine.shift()}.`)
+  }
+  if (deliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(array) {
+  if (array.length === 0) {
+    return "The line is currently empty."
+  }
+  var string = ("The line is currently: ")
+  for (var i = 0; i < array.length; i++) {
+    string = string + (`${i + 1}. ${array[i]}, `)
+  }
+  return string.substring(0, string.length - 2)
 }
