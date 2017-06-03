7b9d14e7513d60c72d8e4d148898eda4b25f219d
Daniel Alviso <dan.alviso2@yahoo.com>
2017-06-03 17:20:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    let firstInLine = katzDeliLine.shift();
+    return `Currently serving ${firstInLine}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    let string = "The line is currently: ";
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      string += `${i + 1}. ${katzDeliLine[i]}, `;
+    }
+    string = string.slice(0, -2); // Remove last two characters from string
+    return string;
+  } else {
+    return "The line is currently empty.";
+  }
+}
