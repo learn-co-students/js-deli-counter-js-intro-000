f568bc5ecadcd46398b51c0c7786949c8091bd41
Chris Smucker <cesmucker@gmail.com>
2017-07-17 22:12:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-};
+function takeANumber(katzDeliLine, newName){
+  katzDeliLine.push(newName)
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
+}
 
 function nowServing(katzDeliLine){
-  if (!katzDeliLine.length){
+  if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
-  return `Currently serving ${katzDeliLine.shift()}.`
 }
 
 function currentLine(katzDeliLine){
-  if (!katzDeliLine.length){
+  if (katzDeliLine.length === 0){
     return "The line is currently empty."
-} else {
-  var results = "The line is currently: "
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    results += (i + 1) + ". " + katzDeliLine[i]
-    if (i != katzDeliLine.length - 1) {
-      results += ", "
+  } else {
+    var peopleInLine = "The line is currently: "
+    for (var i = 0; i < katzDeliLine.length; i++){
+      peopleInLine += `${i + 1}. ${katzDeliLine[i]}`
+      if (katzDeliLine.length > i + 1){
+        peopleInLine += ', '
+      }
     }
+    return peopleInLine
   }
-  return results
-}
 }
