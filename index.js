a26cd8f8661f6b98b8b80434bcfb263067ce01b6
laurkim <laurakeem@gmail.com>
2017-07-25 19:00:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return 'Currently serving ' + katzDeliLine.shift() + '.';
+  }
+}
+
+function currentLine(line) {
+  var people = [];
+  if (line.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    for (var i = 0; i < line.length; i++) {
+      var name = line[i];
+      people.push(`${i + 1}. ${name}`);
+    }
+    console.log(people);
+    return `The line is currently: ${people.join(', ')}`
+  }
+}
