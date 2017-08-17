3074633ed68f9dc52efe0b8592870699cec7e9d7
L North <lnordell@gmail.com>
2017-08-17 16:55:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-var katzDeliLine = [];
+var katzDeli = [];
 
-function takeANumber (katzDeliLine, newPerson) {
-  katzDeliLine.push(newPerson);
-  var yourPosition = katzDeliLine.indexOf(newPerson)+1;
-  return `Welcome, ${newPerson}. You are number ${yourPosition} in line.`
-  //or return `Welcome, ${newPerson}. You are number ${katzDeliLine.indexOf(newPerson) + 1} in line.`;
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing (line){
-  while (line.length > 0){
-// this also works >>    return `Currently serving ${line.shift()}.`;
-  var customerName = line[0];
-  line.shift();
-  return `Currently serving ${customerName}.`;
-// for some reason line.shift() at the end of the loop didnt' work. have to figure out why later.
+function nowServing (line) {
+  while (line.length > 0) {
+    return `Currently serving ${line.shift()}.`;
   }
-  return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!"
 }
 
-function currentLine(line){
-  if (line.length > 0){
-    var myList = [];
-    for (var counter = 0; counter < line.length; counter++) {
-      myList.push (` ${counter+1}. ${line[counter]}`)
-      }
-    return `The line is currently:${myList}`
-    }else{
-    return `The line is currently empty.`
+function currentLine(line) {
+  if (line.length > 0) {
+    var lineArr = [];
+
+    for (let i = 0; i < line.length; i++) {
+      lineArr.push(` ${i+1}. ${line[i]}`);
+    }
+    return `The line is currently:${lineArr}`;
+  } else {
+    return "The line is currently empty."
   }
 }
