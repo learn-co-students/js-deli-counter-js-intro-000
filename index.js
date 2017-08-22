a7043bad4bd12172cfbb820a02fad46232ca8e9f
dragos busuioc <dmihail1994@gmail.com>
2017-08-22 07:40:45 +0000

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function nowServing(katzDeliLine) {
   //if the array with names is empty return
   if(katzDeliLine.length<=0) {
     return "There is nobody waiting to be served!";
-    //else iterate throught names
+    //else iterate throught names ,check if i is 0.If is 0 asign to variable name the name from index 0,then push to lineOrder the mesage with the name and then shift it from the arrays with names
   }else {
       for(var i=0;i<katzDeliLine.length;i++) {
-
+        //when i became greatest than 0 break the loop
         if(i>0) {
           break;
         }
