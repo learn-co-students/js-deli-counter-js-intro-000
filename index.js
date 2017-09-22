a300f92e4adfd1a1705eb6d4a2c80e76c5964001
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-22 15:03:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Define variable katzDeliLine if we want to test using console.log
 var katzDeliLine = [];
 
-// This function takeANumber() adds new people to the katz Deli Line and gives them their place in line
+//This function takeANumber() adds new people to the katz Deli Line and gives them their place in line
 function takeANumber(katzDeliLine, person){
   if (katzDeliLine.length === 0) {
     katzDeliLine.push(person)
@@ -28,9 +28,24 @@ function currentLine(katzDeliLine){
     return "The line is currently empty."
   }else {
     var lineList = []
+    katzDeliLine.forEach(function(name,index){
+       lineList.push(" " + (index+1) + ". " + name)
+    })
+  }
+  return "The line is currently:" + lineList
+}
+
+//currentLine(katzDeliLine)
+/*
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }else {
+    var lineList = []
     for (var i = 0; i < katzDeliLine.length; i++) {
       lineList.push(" "+ (i+1) + ". " + katzDeliLine[i])
     }
   }
   return "The line is currently:" + lineList
 }
+*/
