d0ce88ef603e8915272c8d4d38a4073b256a3dae
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-22 06:19:36 +0000

    Done.

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${counter} in line.`
 }
 function nowServing(katzDeliLine) {
-  if(katzDeliLine.length === 0){
-    return "There is nobody waiting to be served!"
+  if(katzDeliLine.length > 0){
+    let cur = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${cur}.`
   }
   else{
-
-    console.log(`Currently serving ${katzDeliLine[0]}.`)
-    katzDeliLine.shift();
+    return "There is nobody waiting to be served!"
   }
+}
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length > 0) {
+    let queue = ''
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      if (i < katzDeliLine.length - 1) {
+        queue +=`${i + 1}. ${katzDeliLine[i]}, `
+      }else {
+        queue +=`${i + 1}. ${katzDeliLine[i]}`
+      }
+    }
+    return `The line is currently: ${queue}`
+
+  }
+    return "The line is currently empty.";
 }
