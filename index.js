bc182f965cd2e9e582723578cfb9c78adb126aba
nancy sin <nsin@sbcglobal.net>
2018-01-08 17:21:57 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,30 @@ var katzDeli = []
 
 function takeANumber (katzDeliLine, customerName){
   katzDeliLine.push(customerName);
+  var linelength = katzDeliLine.length
   return `Welcome, ${customerName}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return `There is nobody waiting to be served!`
+  }
+ else {
+   var nextPerson = katzDeliLine [0]
+   katzDeliLine.shift()
+   return `Currently serving ${nextPerson}.`
+ }
+}
+
+function currentLine(katzDeliLine){
+    var newLine =[];
+  if (katzDeliLine.length === 0){
+    return `The line is currently empty.`
+}
+  else {
+    for (var i = 0; i < katzDeliLine.length; i++){
+      newLine.push( " " + (i + 1) + ". " + katzDeliLine[i]);
+    }
+    return ("The line is currently:" + newLine)
+  }
 }
