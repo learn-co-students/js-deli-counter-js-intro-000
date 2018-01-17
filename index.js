eaa29f48766fb74309e629b3b965c9edfdefc0ea
Lou Angeles <lou.c.angeles@gmail.com>
2018-01-17 07:12:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
+
+
+var katzDeliLine = [];
+
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+
+nowServing(katzDeliLine); // "Currently serving Ada."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
+
+takeANumber(katzDeliLine, "Matz"); // "3"
+
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
+
+nowServing(katzDeliLine); // "Currently serving Grace."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
