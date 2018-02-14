c64ee928c6e4f500589666e9030bc118b8d50cc3
Alex Segers <a.segers.dev@gmail.com>
2018-02-14 22:59:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+var katzDeliLine = ["Larry", "Gerome"];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  console.log(`You are number ${katzDeliLine.length} in line.`);
+}
+
+fu
