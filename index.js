97e7741d2a8ceb13b068ceb9a0fb3c8b1c860686
Alex Segers <a.segers.dev@gmail.com>
2018-02-15 23:18:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-var katzDeliLine = ["Larry", "Gerome"];
+var katzDeliLine = ["Larry", "Gerome", "Jake"];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  console.log(`You are number ${katzDeliLine.length} in line.`);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-fu
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+     return "The line is currently empty."
+  } else {
+    var string = "The line is currently:"
+
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      string += ` ${i + 1}. ${katzDeliLine[i]},`
+    }
+    string = string.slice(0, -1) // Remove unnecessary ',' at end of str
+
+    return string
+  }
+}
