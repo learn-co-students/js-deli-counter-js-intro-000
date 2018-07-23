e7629a83118be5be98b0023f5e4adb12b840a119
joe fisher <jhanajon@gmail.com>
2018-07-23 16:10:41 +0000

    Done.

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ function takeANumber(line,name) {
 
 
 
-function nowServing(line,name) {
-  if(line.length <= 0) {
-  line.shift()
-} else {
+function nowServing(line) {
+  if(line[0]){
+  return (`Currently serving ${line.shift()}.`)
+}
+ else {
+  if(line.length < 1) {
   return  "There is nobody waiting to be served!"
 }
 }
+}
 
 
-function currentLine(name) {
-  // takeANumber(katzDeliLine, "Ada");
+function currentLine(line) {
+  if(line.length > 0) {
+    var array = []
+    for (var i = 0; i < line.length; i++) {
+      array.push(`${i+1}. ${line[i]}`)
+    }
+    return `The line is currently: ${ array.join(", ") }`
+  } else {
+    if(line.length <= 0) {
+      return "The line is currently empty."
+    }
+  }
 }
