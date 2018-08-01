4ec3c722c136f265646015e36c937560be516546
Alena <i.am.am.629@gmail.com>
2018-08-01 17:46:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function takeANumber(line, name){
-  return `Welcome, ${name}. You are number ${line.length + 1} in line.`
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 function nowServing(line){
-  if line.length > 0 {
+  if (line.length > 0) {
     var i = line.shift()
     return `Currently serving ${i}.`
   } else {
@@ -12,15 +13,13 @@ function nowServing(line){
 }
 
 function currentLine(line){
-  if line.length = 0 {
+  if (line.length === 0) {
     return "The line is currently empty."
-  }
-  var sline = `The line is currently:`
-  for (i = 0, i < line.length, i++){
-    sline += ` ${i + 1}. ${line[i]}`
-    if i != line.length {
-      sline += `,`
+  } else {
+    var sline = `The line is currently: 1. ${line[0]}`
+    for (var i = 1; i < line.length; i++){
+      sline += `, ${i + 1}. ${line[i]}`
     }
+    return sline
   }
-  return sline
 }
