3043e2019bac3c86499beeae85cf964c96f952d0
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-04 01:24:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+ function takeANumber (katzDeliLine, name){
+   katzDeliLine.push(name)
+   return "Welcome, " +name+ ". You are number " +katzDeliLine.length+ " in line."
+ }
+
+function nowServing(katzDeliLine){
+  {
+    if (katzDeliLine.length === 0)
+    return "There is nobody waiting to be served!"
+    else
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine){
+
+  if (katzDeliLine.length === 0){
+  return "The line is currently empty."
+  }
+  else{
+  var numsAndNames = []
+  for (let i = 0, l = katzDeliLine.length; i < l; i++)
+  numsAndNames.push(`${i+1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${numsAndNames.join(', ' )}`
+
+}
