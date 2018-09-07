b5beae25b3e5146c17548bac7ea461172cb44e4d
Brenden Togioka <brentogi@gmail.com>
2018-09-07 07:11:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length > 0) {
+    var info = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    return info;
+  } else {
+    return `There is nobody waiting to be served!`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length < 1) {
+    return 'The line is currently empty.';
+  } else {
+    var line = [];
+    katzDeliLine.forEach(function(name,index) {
+        var num = index + 1;
+        line.push(` ${num}. ${name}`);
+       })
+
+    return `The line is currently:${line}`;
+    }
+
+
+}
