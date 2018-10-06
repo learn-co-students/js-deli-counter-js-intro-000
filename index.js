1e589cb635eb8acbbbf97bc086afdfab2cb51061
Kate Rentschler <nkpe@outlook.com>
2018-10-06 00:26:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+//var katzDeliLine = [];
+function takeANumber(katzDeli, name){
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}
+
+function nowServing(katzDeli){
+
+  if(katzDeli.length>0){
+    return `Currently serving ${katzDeli.splice(0,1)}.`;
+
+  }
+  else{
+    return 'There is nobody waiting to be served!';
+    }
+  }
+
+function currentLine(line){
+
+  if(line.length === 0){
+    return 'The line is currently empty.';
+    }
+
+  else if(line.length > 0){
+    let i = 1;
+    let numLine = [];
+    while(i <= line.length){
+    numLine.push(` ${i}. ${line[i-1]}`);
+    i++;
+    }
+  return `The line is currently:${numLine}`;
+  }
+}
