03796125ddf06eb362ee236b1a20d7649042b929
contact <contact@vishalp.xyz>
2018-10-11 15:33:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var katzDeliLine = [];
 
 function takeANumber (katzDeli, name) {
-  katzDeli.push('${name}');
+  katzDeli.push(name);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 function nowServing (katzDeli) {
   if (katzDeli.length > 0) {
     var nowServing = katzDeli[0];
     katzDeli.shift();
-    return 'Currently serving ${nowServing}.';
+    return `Currently serving ${nowServing}.`;
   } else {
     return 'There is nobody waiting to be served!';
-}
+} }
 function currentLine (katzDeli) {
   if (katzDeli.length === 0) {
-    return 'The line is currently empty';
-  } else {
+    return 'The line is currently empty.'
+  }
 
     var array =[];
-    for (var i = 0; i < line.length; i++){
-      array.push (` ${i+1}. ${line[i]}`);
+    for (var i = 0; i < katzDeli.length; i++){
+      array.push(` ${i+1}. ${katzDeli[i]}`)
     }
-    return 'The line is currently empty' + array;
+    return 'The line is currently:' + array;
   }
