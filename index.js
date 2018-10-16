21f88f75d1a1b7aec7d65b8f52e69e0903528c9e
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-16 13:25:49 +0000

    Done.

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ function nowServing(katzDeliLine) {
    else if (katzDeliLine.length ===  0){
      return "There is nobody waiting to be served!"
    }
+    return `Welcome, ${name[i]}. You are number ${people[i++]} in line.`
+}
+
+function currentLine(line){
+  if (line.length === 0){
+      return ("The line is currently empty.")
+  }
+  var queue = [];
+  for (var i = 1; i <= line.length; i++){
+      queue.push (`${i}. ${line[i-1]}`)
+  }
+  return (`The line is currently: ${queue.join(', ')}`)
 }
