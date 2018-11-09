663b6e1e71b56aa9be4425e1dc9e2a16496fb6ed
Hal Dunn <hallie.dunn1@gmail.com>
2018-11-09 01:02:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function takeANumber(array, customer){
-  array.push(customer)
-  return (`Welcome, ${customer}. You are number ${array.length} in line.`)
-}
-function currentLine(array){
-  if (array && array.length>=1) return (`The line is currently: 1. ${array[0]}, 2. ${array[1]}, 3. ${array[2]}`)
-  else return "The line is currently empty."
+  array.push(customer);
+  return (`Welcome, ${customer}. You are number ${array.length} in line.`);
 }
 function nowServing (array){
-  if (array && array.length >=1){ var placeholder = [array[0]]
-  array.shift()
-    return(`Currently serving ${placeholder[0]}.`)
-  } else return "There is nobody waiting to be served!"
+  if (array && array.length >=1){ var placeHolder = [array[0]];
+  array.shift();
+    return(`Currently serving ${placeHolder[0]}.`);
+  } else return "There is nobody waiting to be served!";
+}
+function currentLine(array){
+  var placeHolder = [];
+    if (array.length >= 1) {
+      while (array.length >= 1){
+    placeHolder.unshift(` ${array.length}. ${array[array.length - 1]}`);
+    array.pop();
+  }return `The line is currently:${placeHolder}`}
+  else if (array.length === 0) {return `The line is currently empty.`}
 }
