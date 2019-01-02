70b8781d526bc018864755feafbe125421092398
Paul Riverain <paulriverain@yahoo.com>
2019-01-02 18:37:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-
 function takeANumber(katzDeliLine, name){
- katzDeliLine.push(name)
+  katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
-    return `Currently serving ${katzDeliLine.shift()}.`
-  } else {
-  return "There is nobody waiting to be served!"
-  }
-}
-function currentLine(katzDeliLine){
-let people = [];
-for (let i = 0; i<katzDeliLine.length; i++){
-  people.push(' '+[i+1]+'. '+ katzDeliLine[i])
-}
-  if (katzDeliLine.length > 0){
-return 'The line is currently:'+ people
+    return 'Currently serving '+ katzDeliLine.shift() +'.'
   }else{
-    return "The line is currently empty."
+    return 'There is nobody waiting to be served!'
   }
+}
+let katzDeliLine = ['steve', 'mike', 'jim']
+function currentLine(katzDeliLine){
+  let line = [];
+  for(let i = 0; i<katzDeliLine.length; i++){
+    line.push(' '+[i+1]+'. '+katzDeliLine[i])
+  }
+  if (katzDeliLine.length >0){
+      return 'The line is currently:'+ line
+    }else{
+      return 'The line is currently empty.'
+    }
 }
