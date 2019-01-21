1f44de6b622fdffaba2e9b4ab3503ac6bfc7853d
Yingqi Chen <chanwk18@hotmail.com>
2019-01-21 08:43:57 +0000

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ function takeANumber(array,name){
    return `Welcome, ${name}. You are number ${array.length} in line.`
  }
 
-function nowServing(array){
-  if (array.length===0){
-    return There is nobody waiting to be served!
-  }else{
-    var name=array[0];
-    array.shift();
-    return `Currently serving ${name}` ;
-  }
+ function nowServing(array){
+   if (array.length===0){
+     return `There is nobody waiting to be served!`
+
+   }else{
+     var name=array[0];
+     array.shift();
+     return `Currently serving ${name}.` ;
+   }
+ }
+
+
+
+
+ function currentLine(array){
+
+   if(!array.length){
+     return `The line is currently empty.`
+   }else{
+     var array2=new Array();
+     for(var i=0;i<array.length;i++){
+     array2.push(`${i+1}. ${array[i]}`)
+ }
+
+     return `The line is currently: ${array2.join(', ')}`
+ }
 }
