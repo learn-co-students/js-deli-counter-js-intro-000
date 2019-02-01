7e1ba9b837908da2408e9b0024bd7671bef01fae
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-02-01 05:33:24 +0000

    Done.

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function nowServing(katzDeli){
 
 function currentLine(line) {
   if (line.length > 0) {
-    let message = "The line is currently: ";
+    let message = "The line is currently:";
     let people_in_line = [];
     for (var i = 0; i < line.length; i++) {
-      let person_in_line = `${i + 1}. ${line[i]}, `;
+      let person_in_line = ` ${i + 1}. ${line[i]}`;
       people_in_line.push(person_in_line);
     }
     return message + people_in_line;
