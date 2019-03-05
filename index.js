d8b64fe0e21d789d37daee274af77d4bceaa125e
Blake A Long <blake.a.long@gmail.com>
2019-03-05 23:05:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine, newName) {
   katzDeliLine.push(newName);
-  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
+    return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
   }
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift()}.`;
 } else { 
-  return "There is nobody waiting to be served!";
+    return "There is nobody waiting to be served!";
 }
 }
 
