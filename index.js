449db3545fadb599282cfce0e313c389df63238f
Sarah Elzinga <sevans924@gmail.com>
2019-03-15 03:01:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 var katzDeliLine = []
 
-function takeANumber(line, name) {
+function takeANumber(katzDeliLine, name) {
 
-  line.push(name)
+  katzDeliLine.push(name)
 
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(line) {
-if (!line.length) {
+function nowServing(katzDeliLine) {
+if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!"
+} else {
+
+return `Currently serving ${katzDeliLine.shift()}.`
+}
 }
 
-return `Currently serving ${line.shift()}.`
-
-}
-
-function currentLine(line) {
-  if (!line.length) {
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
-  }
+  } else {
   const katzLine = []
 
-  for (let i = 0, l = line.length; i < l; i++){
-    katzLine.push(`${i + 1}. ${line[i]}`)
+  for (let i = 0, l = katzDeliLine.length; i < l; i++){
+    katzLine.push(`${i + 1}. ${katzDeliLine[i]}`)
   }
   return `The line is currently: ${katzLine.join(', ')}`
+}
 }
