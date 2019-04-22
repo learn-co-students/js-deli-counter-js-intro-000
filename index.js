d101158f8ecc9f36e3ed0b67fe1bdfd51e848ca5
Patrick B Brown <pbbrown1112@gmail.com>
2019-04-22 16:22:35 +0000

    Done.

--- index.js
+++ index.js
@@ -3,27 +3,24 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-function nowServing(arr1) {
-      if (arr1.length === 0) {
-      return 'There is nobody waiting to be served!';
-    }
-    else {
-      var currentName = arr1[0];
-      arr1.shift();
-      return `Currently serving ${currentName}.`;
-    }
+function nowServing(line) {
+  if (line.length == 0) {
+    return 'There is nobody waiting to be served!';
   }
+  else {
+    return `Currently serving ${line.shift()}.`;
+  }
+}
 
+var Line1 = [];
 function currentLine(line) {
-    if (line.length === 0) {
-      return 'The line is currently empty.';
+  if (line.length == 0) {
+    return `The line is currently empty.`
+  }
+  else {
+    for (var i = 0; i < line.length; i++) {
+      Line1.push(' '+[i+1]+'. '+ line[i]);
     }
-    else  {
-      var arr3 = []
-      for (var i=0; i < line.length; i++) {
-        arr3.push(` ${i+1}. ${line[i]}`);
-    }
-    return 'The line is currently:' + arr3;
-
-    }
+    return `The line is currently:` + Line1;
+  }
 }
