19b82ad465b11603ee5e883f43e41cc11ca45d61
Mai Campbell <jirallc@gmail.com>
2019-04-24 20:46:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeliLine = [];
+
+function takeANumber(line, name) {
+    line.push(name);
+    return `Welcome, ${name}. You are number ${line.length} in line.`;
+
+}
+
+function nowServing(line) {
+    if(!line.length) {
+    return `There is nobody waiting to be served!`;
+  }
+
+    return `Currently serving ${line.shift()}.`
+
+}
+
+function currentLine(line) {
+      if(!line.length) return 'The line is currently empty.';
+
+      const numberAndName = [];
+
+      for (let i = 0; i < line.length; i++) {
+        numberAndName.push(`${i + 1}. ${line[i]}`);
+      }
+      return `The line is currently: ${numberAndName.join(', ')}`
+}
