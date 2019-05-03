d08f65f987cdc3b0cb7a5b047819660b104f1389
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-05-03 05:40:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
-  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
 function nowServing(katzDeliLine){
   if (katzDeliLine>[]){
-    katzDeliLine=katzDeliLine.shift();
-    return `Currently serving Steven.`;
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
-  else {
-    return "There is nobody waiting to be served!"
-  }
+else {
+  return "There is nobody waiting to be served!"
 }
+}
+
 
 function currentLine(katzDeliLine){
   var line=[];
   if (katzDeliLine>[]){
-    for (var i=0; i<katzDeliLine.length; i++){
-      line.push(` ${i+1}. ${katzDeliLine[i]}`)
+    for (let i=0; i<katzDeliLine.length; i++){
+      line.push(` ${i+1}. ${katzDeliLine[i]}`);
     }
-    return 'The line is currently:'+ line;
+      return 'The line is currently:' + line ;
+
+  } else {
+    return "The line is currently empty."
   }
-  else {
-  return "The line is currently empty."
-}
+
 }
