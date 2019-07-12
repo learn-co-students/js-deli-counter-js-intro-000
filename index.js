b72b76be13a042e8d59e719337d4a62da249e6df
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 09:45:17 +0000

    Done.

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ var currentLine = (line) => {
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-    var response = "The line is currently: "
+    var response = "The line is currently:"
     for (let i = 0; i < line.length; i++) {
       if (i === line.length - 1) {
-        response = response + (i + 1) + ". " + line[i]
+        response = `${response} ${i + 1}. ${line[i]}`
       } else {
-        response = response + (i + 1) + ". " + line[i] + ", "
+        response = `${response} ${i + 1}. ${line[i]},`
       }
     }
     return response
