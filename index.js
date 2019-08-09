beff8490085292aeb30525b85dc8e13f111cf882
Geno M <giantpowpow2@gmail.com>
2019-08-09 05:29:33 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name) {
 
 // console.log(takeANumber(['John', 'Montrezl'], 'Billy'))
 
-let pokemon = ['Cyndaquil', 'Bulbasaur', 'Charmander', 'Squirtle'];
+// let pokemon = ['Cyndaquil', 'Bulbasaur', 'Charmander', 'Squirtle'];
 // let pokemon = [];
 
 function nowServing(line) {
@@ -19,7 +19,7 @@ function nowServing(line) {
 // console.log(nowServing(pokemon));
 // console.log(pokemon);
 
-function newPokemonArray(line) {
+function customerLine(line) {
   for (let i = 0; i < line.length; i++) {
     line[i] = `${line.indexOf(line[i])+1}. ${line[i]}`;
   };
@@ -30,9 +30,8 @@ function currentLine(line) {
   if (line[0] === undefined) {
     return "The line is currently empty.";
   } else {
-      return `The line is currently: ${newPokemonArray(line).join(', ')}`;
-      // return `The line is currently: ${line.join(', ')}`;
+    return `The line is currently: ${customerLine(line).join(', ')}`;
   };
 };
 
-console.log(currentLine(pokemon));
+// console.log(currentLine(pokemon));
