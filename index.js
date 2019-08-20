48d9fc6933fc10d3a68dee8c687b925fe18726fb
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-20 05:02:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var katzDeliLine = [];  //decalre array variable
 
-function takeANumber(katzDeliLine, name){   //function that accepts 2 arguments
-  katzDeliLine.push(name);  //add name to array
-  return "Welcome, " + name + ". " + "You are number " + katzDeliLine.length + " in line.";
-  //return message for the name and the last position number in array
+function takeANumber(katzDeliLine, newName) {
+    katzDeliLine.push(newName);
+    return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(x){
-  if (x.length === 0){  //if last position number in array equels 0, meaning it is empty
-    return "There is nobody waiting to be served!";
-  }else{
-    var name = x[0];  //needs clarification
-    x.splice(0, 1);   //needs clarification
-    return "Currently serving " + name + "."
-  }
-}
+function nowServing(katzDeliLine) {
+    if (katzDeliLine.length === 0) {
+        return `There is nobody waiting to be served!`;
+    } else {
+        var currentlyServingName = `Currently serving ${katzDeliLine[0]}.`;
+        katzDeliLine.shift();
+        return currentlyServingName;
 
-function currentLine(x){
-  var line = [];   //variable line is an array
-  if (x.length === 0){  //if last position number in array equels 0, meaning it is empty
-    return "The line is currently empty."
-  } else {
-    for(var i = 0; i < x.length; i++){
-      //for loop, if i = O (place in line) < last postition in array (number), it increments by 1 and  through numbers in array and returns array
-      line += (i + 1) + ". " + x[i] + ", "
     }
-    line = line.slice(0, line.length-2)
-    return "The line is currently: " + line;
-  }
+}
+
+function currentLine(katzDeliLine) {
+    if (katzDeliLine.length === 0) {
+        return "The line is currently empty."
+    } else {
+        var beginText = "The line is currently: ";
+        var wholeText = "";
+        var i = 0;
+        for (i = 0; i < katzDeliLine.length; i++) {
+            wholeText = wholeText + `${i + 1}. ${katzDeliLine[i]}, `
+        }
+        wholeText = beginText + wholeText;
+        return wholeText.slice(0, wholeText.length - 2);
+    }
 }
