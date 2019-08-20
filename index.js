e2b23aba5c9b1d9f1d494856465ee6e84554b3e9
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-20 22:03:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
+var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, newName) {
-    katzDeliLine.push(newName);
-    return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
+function takeANumber(katzDeliLine, newName){
+  katzDeliLine.push(newName);
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(katzDeliLine) {
-    if (katzDeliLine.length === 0) {
-        return `There is nobody waiting to be served!`;
-    } else {
-        var currentlyServingName = `Currently serving ${katzDeliLine[0]}.`;
-        katzDeliLine.shift();
-        return currentlyServingName;
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return `There is nobody waiting to be served!`;
+  } else {
+    var currentlyServingName = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    return currentlyServingName;
+  }
+}
 
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return `The line is currently empty.`;
+  } else {
+    var wholeText = "";
+    var text = "The line is currently: ";
+    for(var i = 0; i < katzDeliLine.length; i++){
+      wholeText = wholeText + `${i + 1}. ${katzDeliLine[i]}, `;
     }
+      wholeText = text + wholeText
+      return wholeText.slice(0, wholeText.length - 2);
+  }
 }
-
-function currentLine(katzDeliLine) {
-    if (katzDeliLine.length === 0) {
-        return "The line is currently empty."
-    } else {
-        var beginText = "The line is currently: ";
-        var wholeText = "";
-        var i = 0;
-        for (i = 0; i < katzDeliLine.length; i++) {
-            wholeText = wholeText + `${i + 1}. ${katzDeliLine[i]}, `
-        }
-        wholeText = beginText + wholeText;
-        return wholeText.slice(0, wholeText.length - 2);
-    }
-}
- 
