05398c860e277321d850d695c77f6559e8750641
zaw z tun <americanarakan@gmail.com>
2019-08-29 16:22:54 +0000

    Done.

--- index.js
+++ index.js
@@ -52,3 +52,23 @@ var currentLine = function (katzDeliLine) {
 }
   return "The line is currently: " + customerAndNumber.join(", ");
 }
+
+var katzDeliLine = [];
+
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+
+currentLine(katzDeliLine, newCustomer); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+
+nowServing(katzDeliLine, "Ada"); // "Currently serving Ada."
+
+currentLine(katzDeliLine, newCustomer ); // "The line is currently: 1. Grace, 2. Kent"
+
+takeANumber(katzDeliLine, "Matz"); // "Welcome, Matz. You are number 1 in line."
+
+currentLine(katzDeliLine, newCustomer); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
+
+nowServing(katzDeliLine, "Grace"); // "Currently serving Grace."
+
+currentLine(katzDeliLine, newCustomer); // "The line is currently: 1. Kent, 2. Matz"
