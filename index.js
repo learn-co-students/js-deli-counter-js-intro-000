09ec3f13e7dd30c05fd016f4a5e4f3b5f376fced
Sunny Niu <sunnyyingli@gmail.com>
2019-09-01 01:46:48 +0000

    Done.

--- index.js
+++ index.js
@@ -13,24 +13,20 @@ function takeANumber(katzDeli, name){
 function nowServing(katzDeli){
   if(katzDeli.length === 0){
     return 'There is nobody waiting to be served!'
-  }else{
-    let newObject = Object.assign({}, katzDeli);
-    katzDeli.shift();
-    return "Currently serving " + newObject[0] + ".";
-
   }
+    return `Currently serving ${katzDeli.shift()}.`
 }
 
 function currentLine(katzDeli){
   if(katzDeli.length === 0){
     return 'The line is currently empty.'
-  }else{
-    let str = 'The line is currently: ';
-    for(let i = 0; i < katzDeli.length; i++){
-      str = str + `${i+1}. ${katzDeli[i]}, `
-    }
-
-    str = str.substring(0, str.length-2);
-    return str;
   }
-}
+
+  const numbersAndNames = [];
+
+  for(let i = 0; i < katzDeli.length; i++){
+      numbersAndNames.push(`${i+1}. ${katzDeli[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
