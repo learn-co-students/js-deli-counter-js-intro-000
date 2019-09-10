4064e6327572e7b0f0f948302e1375258067eea9
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-10 20:49:26 +0000

    Done.

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ return `Currently serving ${name}.`
 function currentLine(line) {
   let i = 0;
   while (i < line.length) {
-    line.push(` `+[i+1]+`. `  + line[i])
+    line.push(`${[i+1]}. ${line[i]}`)
     i++;
   }
   if (line.length === 0) {
     return "The line is currently empty.";
   } else
-  return(`The line is currently:` + line);
+  return(`The line is currently: ${line}`);
 }
