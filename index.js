d5842ab54760c0b4e36968a2f478346240059d12
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-10 20:41:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber( katzDeliLine, name){
+//adds the new name to the array.
+  katzDeliLine.push(name);
+//tell the new person in line his or her number.
+return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing (deliLine){
+let name;
+
+if(deliLine.length === 0){
+  return "There is nobody waiting to be served!"
+}
+
+name = deliLine.shift();
+return `Currently serving ${name}.`
+}
+
+function currentLine(line) {
+  let i = 0;
+  while (i < line.length) {
+    line.push(` `+[i+1]+`. `  + line[i])
+    i++;
+  }
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
