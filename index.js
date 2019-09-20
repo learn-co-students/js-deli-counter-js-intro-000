3ff2963d0dcfec06bdfbf091a0bb69ac2c513c68
melkilic <54556876+melkilic@users.noreply.github.com>
2019-09-19 23:54:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
 
-  console.log("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
-
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 takeANumber(katzDeliLine, "Ada")
