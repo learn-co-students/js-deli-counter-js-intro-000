2340772b000635794b828e2c7daaf52b5c3d8e63
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 17:13:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function takeANumber(lineNumber, name) {
-  return `Welcome, ${name}. You are number ${lineNumber} in line.`;
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  let firstPerson = katzDeliLine[0];
+  if(firstPerson === undefined) {
+    return "There is nobody waiting to be served!";
+  } else {
+    katzDeliLine.shift();
+    return "Currently serving " + firstPerson + '.';
+  }
+}
+
+function currentLine(line) {
+  let newArray = [];
+  if(line.length === 0) {
+    return "The line is currently empty.";
+  }
+
+  for(let i = 0; i < line.length; i++) {
+    newArray.push(`${i+1}. ${line[i]}`);
+  }
+
+  return "The line is currently: " + newArray.join(', ');
 }
