92bb459658d8af882f9a328b098d64ff4b4024e7
Jared Matta <cherokeedreemer@gmail.com>
2019-10-30 00:13:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function takeANumber(currentLine, name){
+  currentLine.push(name)
+  return `Welcome, ${name}. You are number ${currentLine.length} in line.`
+}
+
+
+function nowServing(line) {
+  if (line.length===0){
+    return 'There is nobody waiting to be served!'
+      }
+    return `Currently serving ${line.shift()}.`
+  }
+
+
+function currentLine(line){
+   if (line.length===0){
+     return 'The line is currently empty.'
+   }
+let list= [ ]
+for (let i = 0; i < line.length; i++) {
+list.push( ` ${ i + 1 }. ${line[i]}`)
+}
+return `The line is currently:${list}`
+
+
+}
