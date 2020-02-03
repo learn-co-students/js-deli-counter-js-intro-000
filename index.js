a80a6d4bb46be216bfa91eefb8dbb9395d0935de
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-03 17:55:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var katzDeliLine =[];
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var line = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    return line;
+  } else {return "There is nobody waiting to be served!";}
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var line = "The line is currently:";
+    katzDeliLine.forEach(function(val,index) {
+      line += ` ${index+1}. ${katzDeliLine[index]}`
+      if (index < katzDeliLine.length - 1){line += ',';}
+    });
+    return line;
+  }
+}
