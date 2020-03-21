21d0dc6d32dd4859a36b799ac6162cdf294e0af8
Kevin Minutti <minutti70@gmail.com>
2020-03-21 23:53:56 +0000

    Done.

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return `The line is currently empty.`
-  } else {
-
-    var array = []
+  } else {var array = []
     for (var i = 0; i < katzDeliLine.length; i++) {
       array.push(` ${i+1}. ${katzDeliLine[i]}`)
     }
