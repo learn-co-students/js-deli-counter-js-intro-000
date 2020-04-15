4f9b65e67623b0cabed20326ebf2254939b5d02c
Morgan Matney <morgan.matney89@gmail.com>
2020-04-15 00:59:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  return `Welcome, ${name}. You are number ${line} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  return line[0];
+  if (katzDeliLine > 0) {
+    katzDeliLine.slice(1)
+  } else {
+   return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  while
+}
