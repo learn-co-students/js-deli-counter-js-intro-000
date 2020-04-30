7b196c619be6a2be4f0c8f1357dbd5eecf187b01
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 16:10:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 
@@ -8,7 +8,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return "Currently serving " + katzDeliLine.shift() + ".";
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
 
@@ -19,6 +19,6 @@ function currentLine(katzDeliLine) {
   const lineOrder = [];
 
   for (let i = 0; i < katzDeliLine.length; i++) {
-    lineOrder.push(`${i + 1}. ${katzDeliLine[i]}`)
-  } return "The line is currently: " + lineOrder.join(', ')
+    lineOrder.push(`${i + 1}. ${katzDeliLine[i]}`);
+  } return `The line is currently: ${lineOrder.join(', ')}`
 }
