8387e195345812a8e1beedbf51f9f250d5963ac6
Joshua Hunt <jelijahhunt@gmail.com>
2020-06-03 17:26:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var katzDeliLine = []
 
 function takeANumber(array, name){
-return
+array.push(name)
+return `Welcome, ${name}. You are number ${array.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+if(katzDeliLine.length === 0) {
+ return "There is nobody waiting to be served!"
+} else{
+  return katzDeliLine.shift
+}
 }
