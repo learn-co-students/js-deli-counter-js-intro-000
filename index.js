8d570d04d4a4acca2ac3b2966c57d48eb7a687ab
Joshua Hunt <jelijahhunt@gmail.com>
2020-06-09 04:01:11 +0000

    Done.

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ return `Currently serving ${katzDeliLine.shift()}.`
 }
 
 function currentLine(katzDeliLine){
-  if(katzDeliLine.length > 0) {
-    return `The line is currently ${katzDeliLine.length}`
-  } else {
-    return "The line is currently empty."
-  }
+while(katzDeliLine.length > 0) {
+  return `The line is currently: ${katzDeliLine+=1}`
+}
 }
