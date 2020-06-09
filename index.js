03e3430a57247e60bcf53851e9f74f437f7dff01
Joshua Hunt <jelijahhunt@gmail.com>
2020-06-09 03:27:58 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ function nowServing(katzDeliLine){
 if(katzDeliLine.length === 0) {
  return "There is nobody waiting to be served!"
 } else{
-  return katzDeliLine.shift
+return `Currently serving ${katzDeliLine.shift()}.`
 }
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length > 0) {
+    return `The line is currently ${katzDeliLine.length}`
+  } else {
+    return "The line is currently empty."
+  }
 }
