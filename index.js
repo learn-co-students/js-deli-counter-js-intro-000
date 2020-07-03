e8cb1ad849659d716855f67de5df53d92c9ae918
Kyle Nugent <kln5656@gmail.com>
2020-07-03 18:18:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-var katzDeliLine = [];
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  /* Keep in mind, join() is a a shorthand for looping and building
+   * into a String. */
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
+
+/* var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);            //pushes the new name to the end of the empty array
@@ -20,3 +50,4 @@ function currentLine (katzDeliLine) {
     return("The line is currently: 1. " + katzDeliLine[0] + ", 2. " + katzDeliLine[1] + ", 3. " + katzDeliLine[2]);
   }
 }
+*/
