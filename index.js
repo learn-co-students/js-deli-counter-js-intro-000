04c6e7852bd5a9a2430a6a382191cd9d87128b2a
Colter Ulrich <cju5025@gmail.com>
2020-07-07 18:10:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-
-function currentLine(line) {
-if (!line.length) {
-  return "The line is currently empty."
+function takeANumber (line, name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-const numbersAndNames = []
-
-for (let i = 0, l = line.length; i < l; i++) {
-  numbersAndNames.push(`${i + 1}. ${line[i]}`)
+function nowServing (line){
+  if (line.length > 0){
+    return `Currently serving ${line.shift()}.`
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
 }
 
-return `The line is currently: ${numbersAndNames.join(', ')}`
-};
+function currentLine (line){
+  if (line.length === 0){
+    return 'The line is currently empty.'
+  } else if (line.length > 0){
 
-function nowServing(line) {
-if (!line.length) {
-  return "There is nobody waiting to be served!"
+    var numbersAndNames = [];
+var i;
+    for (i = 0; i < line.length; i++){
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+   }
+      return `The line is currently: ${numbersAndNames.join(', ')}`
+ }
 }
-
-return `Currently serving ${line.shift()}.`
-};
-
-function takeANumber(line, name) {
-line.push(name)
-
-return `Welcome, ${name}. You are number ${line.length} in line.`
-};
