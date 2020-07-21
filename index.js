0602ae7006c0054d7b63d427a7919079e04f75ab
Elizabeth <elizbregman@gmail.com>
2020-07-21 02:29:40 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   let newkatzDeliLine = [];
-  let space = " ";
     if (katzDeliLine.length === 0) {
       return "The line is currently empty.";
     } else {
