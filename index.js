847e99cc303a423ac8177a5c0c0df8dd122caa0c
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-27 17:50:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(line, name){
+  //Pushes name into the array line
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
@@ -11,17 +12,23 @@ function nowServing(line){
     return 'There is nobody waiting to be served!'
   }
 }
-function currentLine(line){
-  if (line.length > 0){
-    var inc = `The line is currently: `
-      for (let i = 0; i < line.length; i++){
-         inc += `${i+1}. ${line[i]}`
-          if (i != line.length-1){
-           inc += `, `;
-          }
-        }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var string = ["The line is currently:"];
+
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      var spot = i + 1;
+      var name = katzDeliLine[i];
+
+      if (i === katzDeliLine.length - 1) {
+        string.push(spot + ". " + name);
       } else {
-    inc = 'The line is currently empty.'
+        string.push(spot + ". " + name + ",");
+      }
     }
-  return inc
+    return string.join(" ");
   }
+}
