e5bc41d4dd13d4fa1989c52e384fb4725ead49d6
Tonia Roganti <toniaroganti@gmail.com>
2020-09-16 16:10:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-
-function takeANumber (linePos,name){
-  linePos.push(name);
-  
-  return `Welcome, ${name}. You are number ${linePos.length} in line.`;
+function takeANumber(katzDeliLine, name){
+    katzDeliLine.push(name);
+    return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
-function nowServing(linePos) {
-  if (linePos.length > 0) {
-    return "Currently serving " + linePos.shift() + ".";
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() + ".";
   } else {
       return "There is nobody waiting to be served!";
   }
 }
-function currentLine(linePos) {
-  if (linePos.length > 0) {
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
     var output = "";
-    for(var i = 0; i < linePos.length; i++) {
-        output += (i + 1) + ". " + linePos[i] + ", ";
+    for(var i = 0; i < katzDeliLine.length; i++) {
+        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
       }
     output = output.slice(0, -2);
     return `The line is currently: ${output}`;
