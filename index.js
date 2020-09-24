87f5adb8e7841f401a001add13c8bd0eba3b96ea
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 22:25:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function takeANumber(line, name) {
-line.push(name)
+  line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 function nowServing(line) {
-  let i = 0
-let n = []
-  if(line.length < 1) {
+var n = []
+  if(line.length == 0) {
     n.push("There is nobody waiting to be served!")
   } else {
     n.push(`Currently serving ${line.shift()}.`)
@@ -15,13 +14,14 @@ let n = []
 }
 
 function currentLine(line) {
-  if (line.length < 1) {
+  if(line.length < 1) {
     return "The line is currently empty."
   } else {
-    let n = `1. ${line[0]}`
-    for (let i = 1 ; i < line.length; i++) {
-     n = n + `, ${i+1}. ${line[i]}`
-}
-    return `The line is currently: ${n}`
-  }
+    var order = `1. ${line[0]}`
+    var i
+    for (i = 1; i < line.length; i++) {
+      order = order + `, ${i+1}. ${line[i]}`
+      }
+      return `The line is currently: ${order}`
+    }
 }
