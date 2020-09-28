5ceafb667a1a2f7cfa23300d72337284467cc5c8
Tonia Roganti <toniaroganti@gmail.com>
2020-09-28 10:48:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-function takeNumber(lineStatus, name){
+function takeANumber(lineStatus, name){
   lineStatus.push(name)
-  lineStatus = lineStatus.lenght + 1
-  return `Welcome ${name}. You are ${lineStatus}`
+  
+  return `Welcome, ${name}. You are number ${lineStatus.length} in line.`
+}
+function nowServing(lineStatus){
+  if(lineStatus.length > 0){
+    
+    return "Currently serving " + lineStatus.shift() + ".";
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+  
+}
+function currentLine(lineStatus){
+  let output = '';
+  if(lineStatus.length > 0){
+    for(let i = 0; i < lineStatus.length; i++){
+    
+    output += (i + 1) + ". " + lineStatus[i] + ", ";
+    
+      }
+      output = output.slice(0, -2)
+      
+        return `The line is currently: ${output}`
+  }else{
+    return "The line is currently empty."
+  }
+
 }
