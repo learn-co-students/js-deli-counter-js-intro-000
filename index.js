bb3e54f87f0e5118743a5f02e50848cdc76faa8b
Tonia Roganti <toniaroganti@gmail.com>
2020-09-28 02:00:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-function takeANumber(katzDeliLine, name){
-    katzDeliLine.push(name);
-    return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-}
-
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine.shift() + ".";
-  } else {
-      return "There is nobody waiting to be served!";
-  }
-}
-
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    var output = "";
-    for(var i = 0; i < katzDeliLine.length; i++) {
-        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
-      }
-    output = output.slice(0, -2);
-    return `The line is currently: ${output}`;
-  } else {
-    return "The line is currently empty.";
-  }
+function takeNumber(lineStatus, name){
+  lineStatus.push(name)
+  lineStatus = lineStatus.lenght + 1
+  return `Welcome ${name}. You are ${lineStatus}`
 }
