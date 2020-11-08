1c17fd8e2e82396c8d24fc873feb59617286d77e
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-08 22:35:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+
 var katzDeliLine = [];
 
 function takeANumber (katzDeliLine, name) {
-  var temp_name = name;
-  katzDeliLine.push(`${name}`);
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(temp_name) + 1} in line.`;
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
@@ -15,19 +15,14 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var line = "";
-  for(let i = 0; i < katzDeliLine.length; i++) {
-    if(i < katzDeliLine.length - 1){
-      line += (` `+[i+1]+`. ` + katzDeliLine[i] + `,`);
+    if(katzDeliLine.length === 0) {
+        return "The line is currently empty."
     }
     else {
-      line += (` `+[i+1]+`. ` + katzDeliLine[i]);
+        var arrayWithIndexAndName = []
+        for (let i = 0; i < katzDeliLine.length; i++) {
+            arrayWithIndexAndName.push(`${i + 1}. ${katzDeliLine[i]}`)
+            }
+        return `The line is currently: ${arrayWithIndexAndName.join(", ")}`
     }
-  }
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty.";
-  }
-  else {
-    return (`The line is currently:` + line);
-  }
 }
