760a977f462e1bb967045e0a9063ec3d3c494334
Chris <chris.mahfouz@hotmail.com>
2020-12-02 14:14:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-function takeANumber(line,name){
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+function takeANumber(currentLine,newName){
+  currentLine.push(newName)
+  return `Welcome, ${newName}. You are number ${currentLine.length} in line.`
+
 }
 function nowServing(katzDeliLine){
   var firstPerson=katzDeliLine.shift()
   if(katzDeliLine.length===0){
-    return  "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!"
   }
   return `Currently serving ${firstPerson}.`
 }
+
 function currentLine(line){
-
+  var newLine='The line is currently:'
   if(line.length===0){
-    return "The line is currently empty."
+    return "The line is currently empty."}
+    for(let i=0;i<line.length;i++)
+    {
+      newLine=`${newLine} ${i+1}. ${line[i]}${i===line.length-1?'':','}`
+    }
+    return newLine
   }
-
-  var currentLineString="The line is currently:"
-  for(let i=0;i<line.length;i++){
-
-   currentLineString = `${currentLineString} ${i+1}. ${line[i]}${i===line.length-1?'':','}`
-
-  }
-
-  return currentLineString
-
-}
