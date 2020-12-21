1e7c058eb44480f2d371319f8bedda0a082c4c0e
Margarita Morozova <ritamorozova@yahoo.com>
2020-12-21 14:18:34 -0800

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-function takeANumber (katzDeliLine, name){
-  katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+function takeANumber (line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length === 0){
-    return "There is nobody waiting to be served!"
-  }else {
-    return `Currently serving ${katzDeliLine.shift()}.`
+  return katzDeliLine.length === 0 ? 'There is nobody waiting to be served!' : `Currently serving ${katzDeliLine.shift()}.`
+}
+
+function currentLine(line){
+ if (line.length === 0){
+   return 'The line is currently empty.'
+  }else{
+    var currentLine = []
+    for(let i =0; i< line.length; i++){
+      currentLine.push(`${i+1}. ${line[i]}`)
+    }
+   return `The line is currently: ${currentLine.join(', ')}`
   }
 }
-
-function currentLine(katzDeliLine){
- var output;
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  }else{
-    var line = [];
-    for (let i =0; i<katzDeliLine.length; i++){
-      line.push(`${i+1}. ${katzDeliLine[i]}`)
-    }//for
-     return `The line is currently: ${line.join(', ')}`
-  }//else
-}
-
-  
