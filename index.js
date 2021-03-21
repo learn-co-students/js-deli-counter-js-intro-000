70c11ec1e81ccd8462b81e1758510fd610f2f839
mmmfang <mae.fang@gmail.com>
2021-03-21 03:28:31 +0000

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ function nowServing(current){
 function currentLine(current){
   if (current.length === 0) {
     return `The line is currently empty.`;
-  } else
-    return `The line is currently: 1. ${current[0]}, 2. ${current[1]}, 3. ${current[2]}`
+  } else {
+
+    //  for (i=0; current.length>0; i++){
+       var newArray = [];
+       var i;
+
+       for (i=0; i < current.length; i++) {
+       var placeInLine = ${i + 1};
+       var nameInLine = current[i];
+       //var newName = `${placeInLine}. ${current[i]}`
+       //newArray = newName.push;
+       console.log(nameInLine);
+       placeInLine++;
+    }
+    //return `The line is currently: 1. ${current[0]}, 2. ${current[1]}, 3. ${current[2]}`
   }
+}
