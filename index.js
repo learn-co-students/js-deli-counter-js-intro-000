2380f38388a4f91d50e9def52d5102e3183b58d0
David P Sanders <davepaulsanders@gmail.com>
2021-03-28 20:09:13 +0000

    Done.

--- index.js
+++ index.js
@@ -10,22 +10,12 @@ katzDeli.push(name)
 // maybe I need a counter to give each name it's own index?  I thought they should index automatically but I'm not sure
 // I think you would need a while loop, because you don't actually know how many they are going to add
 
-//function nowServing(katzDeliLine) {
-//  if (katzDeliLine.length > 0){
-//    var firstInLine = katzDeliLine.shift()
-//    return "Currently serving " + firstInLine + "."
-
-//} else {
-//  return 'There is nobody waiting to be served!'
-//}
-//}
-
-
 function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0){
+    var firstInLine = katzDeliLine.shift()
+    return "Currently serving " + firstInLine + "."
 
-    console.log(katzDeliLine[0])
-    //return `Currently serving .`
-
-//} else {
-//  return 'There is nobody waiting to be served!'
+} else {
+  return 'There is nobody waiting to be served!'
+}
 }
