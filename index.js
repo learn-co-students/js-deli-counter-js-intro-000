c991b275a05c0fa23f5ccac1ead2ffc733caec39
David P Sanders <davepaulsanders@gmail.com>
2021-04-03 15:43:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 
-function takeANumber(katzDeli, name){
-let i = 1
-while (i <= katzDeli.length) {
-katzDeli.push(name)
-  return `Welcome, ${name}. You are number ${i} in line.`
-  i++
-  }
-}
-// maybe I need a counter to give each name it's own index?  I thought they should index automatically but I'm not sure
-// I think you would need a while loop, because you don't actually know how many they are going to add
+function takeANumber(katzLine, name){
+katzLine.push(name)
+  return `Welcome, ${name}. You are number ${katzLine.length} in line.`
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0){
-    var firstInLine = katzDeliLine.shift()
+}
+
+function currentLine(line) {
+   var lineNum = [];
+   if (line.length == 0) {
+     return "The line is currently empty.";
+   }
+   for (var i = 0; i < line.length; i++) {
+     lineNum.push(` ${i+1}. ${line[i]}`);
+ 	}
+   return "The line is currently:" + lineNum;
+
+ }
+
+function nowServing(katzLine) {
+  if (katzLine.length > 0){
+    var firstInLine = katzLine.shift()
     return "Currently serving " + firstInLine + "."
 
 } else {
