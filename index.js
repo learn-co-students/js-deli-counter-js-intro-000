43554d59bdf287700b10397ecc8debf3ed8f98d8
Nathan Huse <nathan.huse@yahoo.com>
2021-05-26 18:25:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 function takeANumber(currentLine, newName) {
   currentLine.push(newName);
-  return `Welcome ${newName}. You are currenly number ${currentLine.length} in line.`;
+  return `Welcome, ${newName}. You are number ${currentLine.length} in line.`;
 }
 
 function nowServing(currentLine) {
-  if(currentLine.length===0) {
+  if(currentLine.length === 0) {
     return 'There is nobody waiting to be served!';
   }
-  return currentLine.shift();
+  return `Currently serving ${currentLine.shift()}.`;
 }
 
 function currentLine(currentLine) {
-  const currentLineString = 'The current line is: ';
+  let currentLineString = 'The line is currently: ';
+  if(currentLine.length === 0) {
+    return 'The line is currently empty.';
+  }
   for(let x = 0; x < currentLine.length; x++) {
-    currentLineString.concat(`${x+1}: ${currentLine[x]}`);
+    currentLineString = currentLineString + `${x+1}. ${currentLine[x]}`;
+    if(x != currentLine.length-1) {
+      currentLineString = currentLineString + ', ';
+    }
   }
   return currentLineString;
 }
